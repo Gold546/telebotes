14967d56d882d6f1a8dd41eede0f0be1f831f86a
marat <derkiliaser@gmail.com>
2026-07-06 22:27:35 +0300

    add structure env,AI

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
-Token='8847151797:AAHIdurNG3f6PZCWL1vMRIdMJRhJjIHBqcQ'
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+Token=os.getenv("BOT_TOKEN", "").strip()
+AI_TOKEN = os.getenv("AI_TOKEN", "").strip()
